fd22272f0bb2c9cdd3a991f61bf780eb81b344b8
didenkokirill <94925492+didenkokirill@users.noreply.github.com>
2023-06-07 02:14:12 +0300

    fix ResetCoord,implement EndGame

--- Assets/Scripts/MoveBall.cs
+++ Assets/Scripts/MoveBall.cs
@@ -2,24 +2,43 @@ using UnityEngine;
 
 public class MoveBall : MonoBehaviour
 {
+    public static MoveBall Instance;
+
     [SerializeField] Rigidbody2D rigidBody;
-    [SerializeField] private float speed;
+
     [SerializeField] private Vector2 direction;
-    
+    [SerializeField] private bool paused = false;
+
+    [SerializeField] private float baseSpeed = 3f, currentSpeed, scaleSpeed = 1.1f;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
     private void Start()
     {
-        direction = new Vector2(Random.Range(0.5f, 1), Random.Range(-0.5f, -1));
+        currentSpeed = baseSpeed;
+        direction = new Vector2(Random.Range(0.5f, 1), Random.Range(0.5f, 1));   
     }
-    void Update()
+
+    private void Update()
     {
-        rigidBody.velocity = direction.normalized * speed;
-      
+        rigidBody.velocity = direction.normalized * currentSpeed;
+
+        if (paused & Input.anyKeyDown) 
+        {
+            Time.timeScale = 1;
+            paused = false;
+        }
     }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             direction.x = -direction.x;
+            currentSpeed *= scaleSpeed;
         }
         if (collision.gameObject.CompareTag("TopWall"))
         {
@@ -28,6 +47,16 @@ public class MoveBall : MonoBehaviour
         if (collision.gameObject.CompareTag("BottomWall"))
         {
             direction.y = -direction.y;
-        }
+        }      
+    }
+
+    public void ResetCoord()
+    {
+        transform.position = Vector3.zero;
+        currentSpeed = baseSpeed;
+        direction = new Vector2(Random.Range(0.5f, 1), Random.Range(0.5f, 1));
+
+        Time.timeScale = 0;
+        paused = true;       
     }
 }
