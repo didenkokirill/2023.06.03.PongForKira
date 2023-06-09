c47c1448c0d9e03739d56f19e6775011165c959f
didenkokirill <94925492+didenkokirill@users.noreply.github.com>
2023-06-09 21:17:46 +0300

    fix code stile
    again(

--- Assets/Scripts/ChangeScore.cs
+++ Assets/Scripts/ChangeScore.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class ChangeScore : MonoBehaviour
 {
-    [SerializeField, Range(1, 2)] private int PlayerID;
+    [SerializeField, Range(1, 2)] private int playerID;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Ball"))
         {
-            ScoreManager.Instance.AddScore(PlayerID);
+            ScoreManager.Instance.AddScore(playerID);
             MoveBall.Instance.ResetCoord();
         }
     }
